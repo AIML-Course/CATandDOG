bfd6de962458cbb138e3192a493127f6bb730324
Wei Chen <weichen@apache.org>
2019-10-18 14:55:28 +0800

    debug-link

--- linker.js
+++ linker.js
@@ -4,19 +4,19 @@ class Link extends React.Component {
       <div>
         <div>
           Cat:
-          <a href="/?c=cat&i=0">_0_</a>.
-          <a href="/?c=cat&i=1">_1_</a>.
-          <a href="/?c=cat&i=2">_2_</a>.
-          <a href="/?c=cat&i=3">_3_</a>.
-          <a href="/?c=cat&i=4">_4_</a>
+          <a href="/CATandDOG/?c=cat&i=0">_0_</a>.
+          <a href="/CATandDOG/?c=cat&i=1">_1_</a>.
+          <a href="/CATandDOG/?c=cat&i=2">_2_</a>.
+          <a href="/CATandDOG/?c=cat&i=3">_3_</a>.
+          <a href="/CATandDOG/?c=cat&i=4">_4_</a>
         </div>
         <div>
           Dog:
-          <a href="/?c=dog&i=0">_0_</a>.
-          <a href="/?c=dog&i=1">_1_</a>.
-          <a href="/?c=dog&i=2">_2_</a>.
-          <a href="/?c=dog&i=3">_3_</a>.
-          <a href="/?c=dog&i=4">_4_</a>
+          <a href="/CATandDOG/?c=dog&i=0">_0_</a>.
+          <a href="/CATandDOG/?c=dog&i=1">_1_</a>.
+          <a href="/CATandDOG/?c=dog&i=2">_2_</a>.
+          <a href="/CATandDOG/?c=dog&i=3">_3_</a>.
+          <a href="/CATandDOG/?c=dog&i=4">_4_</a>
         </div>
       </div>
     );
